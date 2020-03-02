b8ce5889c3d3b042ea788a774b25a3aefe23ab12
1Sunny1 <szymon1999-13@o2.pl>
2020-03-02 14:21:50 +0100

    add gtest library for testing

---
+++ test/main.cpp
@@ -0,0 +1,9 @@
+#include "tst_ischaracterequaltocode.h"
+
+#include <gtest/gtest.h>
+
+int main(int argc, char *argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
