1c449fc0932e824455f960c6f2d0682a95338b9a
Cyberah <szymon1999-13@o2.pl>
2020-06-16 13:42:31 +0200

    add application icon

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@ int main(int argc, char *argv[])
 
     QFontDatabase::addApplicationFont(":/font/font/QuiteMagicalRegular-8VA2.ttf");
     QApplication::setFont(QFont("Quite Magical", 24, 1));
+    a.setWindowIcon(QIcon(":/img/img/mc_icon.png"));
 
     w.show();
     return a.exec();
