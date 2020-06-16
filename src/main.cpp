00bee12f143eeef8c91d544bb45f0c186d5ce8af
Cyberah <szymon1999-13@o2.pl>
2020-06-16 16:21:08 +0200

    add window title

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
     QFontDatabase::addApplicationFont(":/font/font/QuiteMagicalRegular-8VA2.ttf");
     QApplication::setFont(QFont("Quite Magical", 24, 1));
     a.setWindowIcon(QIcon(":/img/img/mc_icon.png"));
-
+    w.setWindowTitle("This lil converter ain't much, but honest work C:");
     w.show();
     return a.exec();
 }
